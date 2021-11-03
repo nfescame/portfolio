f575cfd1995315b859da2b2ec0d2df201bc11ea9
nilton <nfescame@gmail.comq:qclear>
2021-11-03 18:02:43 -0300

    :bug:correção de bug funcionalidade aside sobre

--- script.js
+++ script.js
@@ -16,37 +16,7 @@ document
     document.querySelector(".aside-sobre").classList.toggle("show-sobre")
   );
 document
-  .querySelector(".icon-close")
+  .querySelector(".icon-close-sobre")
   .addEventListener("click", () =>
     document.querySelector(".aside-sobre").classList.toggle("show-sobre")
   );
-
-// document.querySelector("#qtde").addEventListener("change", atualizarPreco);
-// document.querySelector("#js").addEventListener("change", atualizarPreco);
-// document
-//   .querySelector("#layout-sim")
-//   .addEventListener("change", atualizarPreco);
-// document
-//   .querySelector("#layout-nao")
-//   .addEventListener("change", atualizarPreco);
-// document.querySelector("#prazo").addEventListener("change", () => {
-//   const prazo = document.querySelector("#prazo").value;
-//   document.querySelector(
-//     "label[for=prazo]"
-//   ).innerHTML = `Prazo: ${prazo} semanas`;
-//   atualizarPreco();
-// });
-
-// function atualizarPreco() {
-//   const qtde = document.querySelector("#qtde").value;
-//   const temjs = document.querySelector("#js").checked;
-//   const comLayout = document.querySelector("#layout-sim").checked;
-//   const prazo = document.querySelector("#prazo").value;
-
-//   let preco = qtde * 200;
-//   temjs ? (preco *= 1.1) : null;
-//   comLayout ? (preco += 500) : null;
-//   let taxaDeUrgencia = 1 - prazo * 0.1;
-//   preco *= 1 + taxaDeUrgencia;
-//   document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
-// }
